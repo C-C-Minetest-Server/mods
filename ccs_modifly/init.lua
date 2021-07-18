fc29e12f1b09d635469fd482c1a40283c68a39a1
Yiu Man Ho <yiufamily.hh@gmail.com>
2021-07-18 23:00:48 +0800

    Slope not block train

--- ccs_modifly/init.lua
+++ ccs_modifly/init.lua
@@ -2,6 +2,9 @@ local changes = {
 	["streets:bigpole"] = {
 		groups = { cracky = 1, level = 2, bigpole = 1, not_blocking_trains = 1,},
 	},
+	["currency:minegeld_100"] = {
+		inventory_image = "minegeld_100_mod.png",
+	},
 }
 
 local func_changes = {}
@@ -15,9 +18,40 @@ local func_changes = {}
 end, {
 	groups = { not_blocking_trains = 1,},
 }})]]
+local for_item = {
+	function(nname)
+		local ndef = table.copy(minetest.registered_items[nname])
+		if not ndef then return end
+		ndef.name = nil
+		ndef.type = nil
+		if ndef.mod_origin == "moreblocks" and ndef.drawtype == "mesh" then
+			-- HACK! TODO: after slope group appears, use that to seperate those nodes
+			if not ndef.groups then ndef.groups = {} end
+			ndef.groups.not_blocking_trains = 1
+			minetest.override_item(nname,ndef)
+		end
+	end,
+	function(nname)
+		local ndef = table.copy(minetest.registered_items[nname])
+		if not ndef then return end
+		ndef.name = nil
+		ndef.type = nil
+		if ndef.mod_origin == "trainblocks" then
+			if not ndef.groups then ndef.groups = {} end
+			ndef.groups.not_blocking_trains = 1
+			minetest.override_item(nname,ndef)
+		end
+	end,
+}
 
 for k,v in pairs(changes) do
 	minetest.override_item(k,v)
+end
+
+for k,v in pairs(for_item) do
+	for x,y in pairs(minetest.registered_items) do
+		v(x)
+	end
 end
 
 for k,v in pairs(func_changes) do
