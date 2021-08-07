2c2c52b8cc6a2942c2c93173d3b46aa92f5f6641
Yiu Man Ho <yiufamily.hh@gmail.com>
2021-08-07 20:32:51 +0800

    BUGFIX

--- ccs_log/init.lua
+++ ccs_log/init.lua
@@ -1,6 +1,6 @@
 local old_set_player_privs = minetest.set_player_privs
 function minetest.set_player_privs(name,privs)
 	local cmod = minetest.get_current_modname()
-	minetest.log("action",(("Mod " .. cmod) or "Unknown Mod") .. "set player " .. name .. "privs to " .. dump(privs))
+	minetest.log("action",(cmod and ("Mod " .. cmod) or "Unknown Mod") .. "set player " .. name .. "privs to " .. dump(privs))
 	return old_set_player_privs(name,privs)
 end
