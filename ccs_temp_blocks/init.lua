71e120e26d387f8b3d3cdf668131e6911d1bda9e
Yiu Man Ho <yiufamily.hh@gmail.com>
2021-10-25 17:36:44 +0800

    bugfix

--- ccs_temp_blocks/init.lua
+++ ccs_temp_blocks/init.lua
@@ -1,16 +1,25 @@
+local function air(pos)
+	minetest.set_node(pos,{name="air"})
+end
 minetest.register_node("ccs_temp_blocks:temp_block_water", {
 	destription = "Moderation Temp Block (Liquid Pointable)",
 	drawtype = "airlike",
-	on_construct = function (pos)
-		minetest.set_node({name="air"})
-	end,
+	on_construct = air,
+	on_punch = air,
+	on_rightclick = air,
+	groups = {cracky = 1, handly = 1},
 	liquids_pointable = true,
+	paramtype = "light",
+	sunlight_propagates = true,
 })
 
 minetest.register_node("ccs_temp_blocks:temp_block_water", {
 	destription = "Moderation Temp Block",
 	drawtype = "airlike",
-	on_construct = function (pos)
-		minetest.set_node({name="air"})
-	end,
+	on_construct = air,
+	on_punch = air,
+	on_rightclick = air,
+	groups = {cracky = 1, handly = 1},
+	paramtype = "light",
+	sunlight_propagates = true,
 })
